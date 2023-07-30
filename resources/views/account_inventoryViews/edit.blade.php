002573a3d51510332818480ea3f15e91a791a2c9
Dedo_Finger2 <antonioimportant@gmail.com>
2023-07-30 17:52:12 -0300

    all views structured

---
+++ resources/views/account_inventoryViews/edit.blade.php
@@ -0,0 +1,16 @@
+
+{{-- Pegando o layout da página --}}
+@extends('layouts.page')
+
+{{-- Título da página --}}
+@section('title', 'xxx')
+{{-- Indica qual parte da navbar o usuário se encontra no momento --}}
+@section('xxx', 'active')
+
+{{-- Conteúdo da página --}}
+@section('content')
+
+    <h1>Editando xxx</h1>
+    <hr>
+
+@endsection
